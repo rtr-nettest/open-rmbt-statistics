c44f8e77cc171532220884c1fc91e5fc20ef3aae
oleksis <sendtosemenov@gmail.com>
2021-11-21 15:36:45 +0100

    Run without datasource add version log

--- src/main/java/com/example/statisticserver/controller/ApplicationVersionController.java
+++ src/main/java/com/example/statisticserver/controller/ApplicationVersionController.java
@@ -7,12 +7,14 @@ import com.example.statisticserver.service.ApplicationVersionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("Version")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ApplicationVersionController {
 
     private final ApplicationVersionService applicationVersionService;
@@ -20,6 +22,7 @@ public class ApplicationVersionController {
     @ApiOperation(value = "Get version of application")
     @GetMapping(URIConstants.VERSION)
     public ApplicationVersionResponse getApplicationVersion() {
+        log.info("Started /version endpoint");
         return applicationVersionService.getApplicationVersion();
     }
 }
