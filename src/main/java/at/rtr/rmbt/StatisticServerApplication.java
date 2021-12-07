a043278a78b2e5f62aea0ab7fefb5975fad96a20
oleksis <oleksis@jfrog.com>
2021-12-07 17:51:22 +0200

    Update git plugin

--- src/main/java/at/rtr/rmbt/StatisticServerApplication.java
+++ src/main/java/at/rtr/rmbt/StatisticServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:git.properties"})
 public class StatisticServerApplication {
 
     public static void main(String[] args) {
