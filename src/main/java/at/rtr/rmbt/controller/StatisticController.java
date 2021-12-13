a4cb9131f6cbb2307252fd44581b534bb272385a
oleksis <sendtosemenov@gmail.com>
2021-12-13 07:50:36 +0100

    Fix swagger, change logback config

--- src/main/java/at/rtr/rmbt/controller/StatisticController.java
+++ src/main/java/at/rtr/rmbt/controller/StatisticController.java
@@ -5,15 +5,11 @@ import at.rtr.rmbt.request.StatisticRequest;
 import at.rtr.rmbt.service.StatisticService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @Slf4j
