2a1981f3c0332b3e534524c144a06f15efc6fd6c
oleksis <sendtosemenov@gmail.com>
2021-12-24 07:57:03 +0100

    Fix pdf export URL

--- src/main/java/at/rtr/rmbt/constant/URIConstants.java
+++ src/main/java/at/rtr/rmbt/constant/URIConstants.java
@@ -7,5 +7,5 @@ public interface URIConstants {
     String EXPORT_PNG = "/{lang}/{open_test_uuid}/{size}.png";
     String OPEN_TEST_BY_UUID = "/opentests/O{open_test_uuid}";
     String EXPORT_OPEN_DATA = "/export/netztest-opendata-{year}-{month}.{format}";
-    String EXPORT_PDF = "/export/pdf/{lang}";
+    String EXPORT_PDF = "/export/pdf";
 }
