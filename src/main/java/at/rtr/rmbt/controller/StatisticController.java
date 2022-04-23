5af26a2daf40f038ba1584950c9f8882e46cd5a2
oleksis <sendtosemenov@gmail.com>
2022-04-23 19:24:04 +0200

    Moving to spring boot 2.6.6

--- src/main/java/at/rtr/rmbt/controller/StatisticController.java
+++ src/main/java/at/rtr/rmbt/controller/StatisticController.java
@@ -3,9 +3,7 @@ package at.rtr.rmbt.controller;
 import at.rtr.rmbt.constant.URIConstants;
 import at.rtr.rmbt.request.StatisticRequest;
 import at.rtr.rmbt.service.StatisticService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,25 +19,10 @@ public class StatisticController {
 
     private final StatisticService statisticService;
 
-    @ApiOperation(value = "Statistics",
-            nickname = "statistics")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "lang", value = "The language that should be exported.", dataType = "string", example = "en", paramType = "body", required = false),
-            @ApiImplicitParam(name = "quantile", value = "Quantile", dataType = "double", example = "0.5", paramType = "body", required = false),
-            @ApiImplicitParam(name = "month", value = "The month that should be calculated.", dataType = "int", example = "1", paramType = "body", required = false),
-            @ApiImplicitParam(name = "duration", value = "The duration that should be calculated.", dataType = "int", example = "1", paramType = "body", required = false),
-            @ApiImplicitParam(name = "max_devices", value = "The number of device that should be calculated.", dataType = "int", example = "1", paramType = "body", required = false),
-            @ApiImplicitParam(name = "type", value = "The type of measurement", dataType = "string", example = "mobile", paramType = "body", required = false),
-            @ApiImplicitParam(name = "network_type_group", value = "Type of the network, e.g. MOBILE, LAN, WLAN.", dataType = "string", paramType = "body", required = false),
-            @ApiImplicitParam(name = "location_accuracy", value = "Estimation of accuracy of client location in meters", dataType = "string", paramType = "body", required = false),
-            @ApiImplicitParam(name = "country", value = "The country of measurement in ISO 3166.", dataType = "string", example = "en", paramType = "body", required = false),
-            @ApiImplicitParam(name = "user_server_selection", value = "Legacy", dataType = "boolean", paramType = "body", required = false),
-            @ApiImplicitParam(name = "endDate", value = "UTC date and time when calculation was ended yyyy-MM-dd HH:mm:ss", dataType = "string", paramType = "body", required = false),
-            @ApiImplicitParam(name = "province", value = "Code of province gkz bev", dataType = "int", paramType = "body", required = false),
-            @ApiImplicitParam(name = "timezone", value = "Timezone", example = "Europe/Vienna", dataType = "string", paramType = "body", required = false)
-    })
+    @Operation(summary = "Statistics",
+            operationId = "statistics")
     @PostMapping(value = URIConstants.STATISTICS, produces = "application/json; charset=utf-8")
-    public String getStatistics(@Valid @RequestBody StatisticRequest statisticRequest) {
+    public String getStatistics(@Valid @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody StatisticRequest statisticRequest) {
         return statisticService.getStatistics(statisticRequest);
     }
 }
--- src/main/java/at/rtr/rmbt/request/StatisticRequest.java
+++ src/main/java/at/rtr/rmbt/request/StatisticRequest.java
@@ -2,6 +2,7 @@ package at.rtr.rmbt.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,47 +14,61 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "Statistic request")
 public class StatisticRequest {
 
     @JsonProperty(value = "capabilities")
     private final CapabilitiesRequest capabilitiesRequest;
 
     @JsonProperty(value = "language")
+    @Schema(type = "string", example = "en", description = "The language that should be exported.")
     private final String language;
 
     @JsonProperty(value = "quantile")
+    @Schema(description = "Quantile", type = "number", example = "0.5")
     private final Double quantile;
 
     @JsonProperty(value = "months")
+    @Schema(description = "The month that should be calculated.", type = "integer", example = "1")
     private final Integer months;
 
     @JsonProperty(value = "duration")
+    @Schema(description = "The duration that should be calculated.", type = "integer", example = "1")
     private final Integer duration;
 
     @JsonProperty(value = "max_devices")
+    @Schema(description = "The number of device that should be calculated.", type = "integer", example = "1")
     private final Integer maxDevices;
 
     @JsonProperty(value = "type")
+    @Schema(description = "The type of measurement", type = "string", example = "mobile")
     private final String type;
 
     @JsonProperty(value = "network_type_group")
+    @Schema(description = "Type of the network, e.g. MOBILE, LAN, WLAN.", type = "string")
     private final String networkTypeGroup;
 
     @JsonProperty(value = "location_accuracy")
+    @Schema(description = "Estimation of accuracy of client location in meters", type = "string")
     private final Double locationAccuracy;
 
     @JsonProperty(value = "country")
+    @Schema(description = "The country of measurement in ISO 3166.", type = "string", example = "en")
     private final String country;
 
     @JsonProperty(value = "user_server_selection")
+    @Schema(description = "Legacy", type = "boolean")
     private final boolean userServerSelection;
 
     @JsonProperty(value = "end_date")
+    @Schema(description = "UTC date and time when calculation was ended yyyy-MM-dd HH:mm:ss", type = "string")
     private final String endDate;
 
     @JsonProperty(value = "province")
+    @Schema(description = "Code of province gkz bev", type = "integer")
     private final Integer province;
 
     @JsonProperty(value = "timezone")
+    @Schema(description = "Timezone", type = "string", example = "Europe/Vienna")
     private final String timezone;
 }
