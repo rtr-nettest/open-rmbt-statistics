cb60634fea7eadb1f3189138c97f1535f5aa0124
Thomas Schreiber <thomas.schreiber@gmx.at>
2022-05-20 10:55:22 +0200

    fix classification values

--- src/main/java/at/rtr/rmbt/dto/Classification.java
+++ src/main/java/at/rtr/rmbt/dto/Classification.java
@@ -18,27 +18,27 @@ package at.rtr.rmbt.dto;
 
 public final class Classification
 {
-    public static final int[] THRESHOLD_UPLOAD = { 10000, 1000, 500 }; // 10Mbit/s, 1Mbit/s, 500kbit/s
+    public static final int[] THRESHOLD_UPLOAD = { 50000, 5000, 2500 };          // 100Mbit/s, 10Mbit/s, 5Mbit/s
     public static final String[] THRESHOLD_UPLOAD_CAPTIONS = { "1", "0.5" };
-    
-    public static final int[] THRESHOLD_DOWNLOAD = { 30000, 2000, 1000 }; // 30Mbit/s, 2Mbit/s, 1Mbit/s
+
+    public static final int[] THRESHOLD_DOWNLOAD = { 100000, 10000, 5000 };      // 50Mbit/s, 5Mbit/s, 2.5Mbit/s
     public static final String[] THRESHOLD_DOWNLOAD_CAPTIONS = { "2", "1" };
-    
+
     public static final int[] THRESHOLD_PING = { 10000000, 25000000, 75000000 }; // 10ms, 25ms, 75ms
     public static final String[] THRESHOLD_PING_CAPTIONS = { "25", "75" };
-    
+
     // RSSI limits used for 2G,3G (and 4G when RSSI is used)
-    // only odd values are reported by 2G/3G 
+    // only odd values are reported by 2G/3G
     public static final int[] THRESHOLD_SIGNAL_MOBILE = { -75, -85, -101 }; // -75 is still ultra-green, -85 is still green, -101 is still yellow
     public static final String[] THRESHOLD_SIGNAL_MOBILE_CAPTIONS = { "-85", "-101" };
-    
+
     // RSRP limit used for 4G
     public static final int[] THRESHOLD_SIGNAL_RSRP = { -85, -95, -111 };
 
     // RSSI limits used for Wifi
     public static final int[] THRESHOLD_SIGNAL_WIFI = { -51, -61, -76 };
     public static final String[] THRESHOLD_SIGNAL_WIFI_CAPTIONS = { "-61", "-76" };
-    
+
 
     public static int classify(final int[] threshold, final long value, int classificationItems)
     {
